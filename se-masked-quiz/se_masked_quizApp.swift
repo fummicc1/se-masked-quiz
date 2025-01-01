e1c31d38d4fe8676131add01fe6f9e2f3d519cfd
fummicc1 <fumiya.tennis1@gmail.com>
2025-01-01 17:06:36 +0900

    Initial Commit

---
+++ se-masked-quiz/se_masked_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  se_masked_quizApp.swift
+//  se-masked-quiz
+//
+//  Created by Fumiya Tanaka on 2025/01/01.
+//
+
+import SwiftUI
+
+@main
+struct se_masked_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ se-masked-quizTests/se_masked_quizTests.swift
@@ -0,0 +1,16 @@
+//
+//  se_masked_quizTests.swift
+//  se-masked-quizTests
+//
+//  Created by Fumiya Tanaka on 2025/01/01.
+//
+
+import Testing
+
+struct se_masked_quizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
