2233f7afa11cb37b8ec24b38bb1be3914e7a8bc9
fummicc1 <fumiya.tennis1@gmail.com>
2025-01-05 17:18:57 +0900

    Fix when showing link on the web.

--- se-masked-quiz/ContentView.swift
+++ se-masked-quiz/ContentView.swift
@@ -8,45 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     @Environment(\.seRepository) var repository
     @State private var proposals: [SwiftEvolution] = []
-
+    @State private var modalWebUrl: URL?
+    
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(proposals) { proposal in
-                    NavigationLink(value: proposal) {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                MarkdownText(proposal.title)
-                                    .font(.headline)
-                                Text("#\(Int(proposal.proposalId) ?? 0)")
-                                    .font(.caption)
+        GeometryReader { proxy in
+            NavigationStack {
+                List {
+                    ForEach(proposals) { proposal in
+                        NavigationLink(value: proposal) {
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    MarkdownText(proposal.title)
+                                        .font(.headline)
+                                    Text("#\(Int(proposal.proposalId) ?? 0)")
+                                        .font(.caption)
+                                }
+                                MarkdownText(proposal.status ?? "")
+                                    .font(.subheadline)
+                                MarkdownText(proposal.authors)
+                                    .font(.subheadline)
+                                MarkdownText(proposal.reviewManager ?? "")
+                                    .font(.subheadline)
                             }
-                            MarkdownText(proposal.status ?? "")
-                                .font(.subheadline)
-                            MarkdownText(proposal.authors)
-                                .font(.subheadline)
-                            MarkdownText(proposal.reviewManager ?? "")
-                                .font(.subheadline)
                         }
                     }
                 }
-            }
-            .navigationTitle("Swift Evolution")
-            .navigationDestination(for: SwiftEvolution.self) { hashable in
-                DefaultWebView(htmlContent: hashable.content)
+                .navigationTitle("Swift Evolution")
+                .navigationDestination(for: SwiftEvolution.self) { hashable in
+                    DefaultWebView(
+                        htmlContent: .string(hashable.content),
+                        onNavigate: { url in
+                            modalWebUrl = url
+                        }
+                    )
                     .navigationTitle(hashable.title)
+                }
+            }
+            .task {
+                do {
+                    proposals = try await repository.fetch()
+                } catch {
+                    assertionFailure(String(describing: error))
+                }
+            }
+            .sheet(item: $modalWebUrl) { url in
+                VStack(spacing: 0) {
+                    HStack {
+                        Text(url.absoluteString)
+                        Spacer()
+                        Button("Close") {
+                            modalWebUrl = nil
+                        }
+                    }
+                    .padding(8)
+                    DefaultWebView(htmlContent: .url(url)) {
+                        modalWebUrl = $0
+                    }
+                    .frame(
+                        width: proxy.size.width * 0.8,
+                        height: proxy.size.height * 0.8
+                    )
+                }
             }
         }
-        .task {
-            do {
-                proposals = try await repository.fetch()
-            } catch {
-                assertionFailure(String(describing: error))
-            }
-        }
+    }
+}
+
+extension URL: @retroactive Identifiable {
+    public var id: String {
+        absoluteString
     }
 }
 
--- se-masked-quiz/DefaultWebView.swift
+++ se-masked-quiz/DefaultWebView.swift
@@ -8,6 +8,38 @@
 import SwiftUI
 import WebKit
 
+enum HTMLContent {
+    case string(String)
+    case url(URL)
+    
+    var content: String {
+        get async {
+            switch self {
+            case .string(let string):
+                return string
+            case .url(let url):
+                // Launch a task to load on background thread.
+                let loadingTask = Task {
+                    try Data(contentsOf: url)
+                }
+                guard let data = try? await loadingTask.value else {
+                    return ""
+                }
+                return String(data: data, encoding: .utf8) ?? ""
+            }
+        }
+    }
+    
+    var url: URL? {
+        switch self {
+        case .url(let url):
+            return url
+        default:
+            return nil
+        }
+    }
+}
+
 #if canImport(UIKit)
 import UIKit
 
@@ -34,22 +66,55 @@ import AppKit
 struct DefaultWebView: NSViewRepresentable {
     typealias NSViewType = WKWebView
     
-    let htmlContent: String
+    let htmlContent: HTMLContent
+    let onNavigate: (URL) -> Void
 
     func makeNSView(context: Context) -> NSViewType {
         let view = NSViewType()
-        view.loadHTMLString(parse(html: htmlContent), baseURL: nil)
+        view.navigationDelegate = context.coordinator
+        Task {
+            await view.loadHtmlContent(htmlContent)
+        }
         return view
     }
 
     func updateNSView(_ nsView: WKWebView, context: Context) {
-        nsView.loadHTMLString(parse(html: htmlContent), baseURL: nil)
+        Task {
+            await nsView.loadHtmlContent(htmlContent)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(onNavigate: onNavigate)
+    }
+    
+    final class Coordinator: NSObject {
+        let onNavigate: (URL) -> Void
+        
+        init(onNavigate: @escaping (URL) -> Void) {
+            self.onNavigate = onNavigate
+        }
     }
 }
 #endif
 
+fileprivate extension WKWebView {
+    func loadHtmlContent(_ htmlContent: HTMLContent) async {
+        if let url = htmlContent.url {
+            load(URLRequest(url: url))
+        } else {
+            loadHTMLString(await parse(html: htmlContent), baseURL: nil)
+        }
+    }
+}
+
 // ref: https://designcode.io/swiftui-advanced-handbook-code-highlighting-in-a-webview
-fileprivate func parse(html: String) -> String {
+fileprivate func parse(html: HTMLContent) async -> String {
+    let htmlContent = await html.content
+    if case .url = html {
+        // if the content is from URL, we don't need to add any styling.
+        return htmlContent
+    }
     let htmlStart = """
         <HTML>
         <HEAD>
@@ -92,7 +157,7 @@ fileprivate func parse(html: String) -> String {
     """
 
     let codeRegex = "<code.*?>"
-    let contentWithCodeStyling = html.replacingOccurrences(
+    let contentWithCodeStyling = htmlContent.replacingOccurrences(
         of: codeRegex,
         with: "$0",
         options: .regularExpression,
@@ -109,4 +174,17 @@ fileprivate func parse(html: String) -> String {
     """
 
     return htmlStart + contentWithCodeStyling + htmlEnd
+}
+
+extension DefaultWebView.Coordinator: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+        let isOriginal = navigationAction.request.url?.absoluteString == "about:blank"
+        if isOriginal {
+            return .allow
+        }
+        if let destinationURL = navigationAction.request.url {
+            onNavigate(destinationURL)
+        }
+        return webView.url?.absoluteString == "about:blank" ? .cancel : .allow
+    }
 }
